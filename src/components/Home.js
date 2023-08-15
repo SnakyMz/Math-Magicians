06001a62f8270b9fea2181855dd8b2ac7b8681e9
SnakyMz <Soban.syeed@outlook.com>
2023-08-16 01:29:45 +0530

    Add Home

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <section id="home">
+      <h2>Welcome to our page</h2>
+      <p>
+        This webpage contains a calculator that perfomrs basic calculations.
+        You can perform addition, subtraction, multiplication, division, and percentage operations.
+        The &quot;AC&quot; button resets the calculations to zero.
+        This webpage also contains a quote section.
+        The Quote section generates a random quote on load.
+      </p>
+    </section>
+  );
+}
+
+export default Home;
