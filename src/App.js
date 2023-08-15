3eb09886c84a5aa1544c98ed138c1057237de1d8
SnakyMz <Soban.syeed@outlook.com>
2023-08-16 01:44:00 +0530

    Add some style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 import Quotes from './components/Quotes';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Home />
       <Calculator />
       <Quotes />
     </div>
