4bd712a7f3963dc90c344908fa51906aa99ce71b
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-17 02:37:13 +0300

    Add data to calculate.test.js

---
+++ src/__tests__/calculate.test.js
@@ -0,0 +1,45 @@
+import calculate from "../logic/calculate";
+describe('Testing calculation inputs', () => {
+  test('Should nullify the operation', () => {
+    const obj = {
+      total: '50',
+      next: '10',
+      operation: '+',
+    };
+    const cal = calculate(obj, 'AC');
+    expect(cal).toEqual(
+      {
+        total: null,
+        next: null,
+        operation: null,
+      }
+    );
+  });
+  test('Should return addition operation', () => {
+    const obj = {
+      total: '50',
+      next: '10',
+      operation: '+',
+    };
+    const cal = calculate(obj, '+');
+    expect(cal.operation).toEqual('+');
+  });
+  test('Should return negative value', () => {
+    const obj = {
+      total: null,
+      next: '10',
+      operation: '+/-',
+    };
+    const cal = calculate(obj, '+/-');
+    expect(cal.next).toEqual('-10');
+  });
+  test('Should return decimal operation', () => {
+    const obj = {
+      total: '50',
+      next: '10',
+      operation: '.',
+    };
+    const cal = calculate(obj, '.');
+    expect(cal.operation).toEqual('.');
+  });
+});
